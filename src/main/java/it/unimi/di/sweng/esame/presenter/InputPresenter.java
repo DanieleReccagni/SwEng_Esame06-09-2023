ce4ca9eaf689dabd9466af7ff1381cd60e4e7cd1
Daniele Reccagni <daniele.reccagni1@studenti.unimi.it>
2023-09-06 17:09:47 +0200

    VERDE: implementato test testCodiceNonValido

--- src/main/java/it/unimi/di/sweng/esame/presenter/InputPresenter.java
+++ src/main/java/it/unimi/di/sweng/esame/presenter/InputPresenter.java
@@ -19,7 +19,19 @@ public class InputPresenter implements Presenter {
     @Override
     public void action(String text1, String text2) {
         if (text1.equals("") || text2.equals("")) throw new IllegalArgumentException();
+
         String[] splittata = text2.split(":");
+
+        if (splittata[0].length() != 5 ||
+                splittata[0].charAt(0) < 65 || splittata[0].charAt(0) > 90 ||
+                splittata[0].charAt(1) < 65 || splittata[0].charAt(1) > 90 ||
+                splittata[0].charAt(2) < 48 || splittata[0].charAt(2) > 57 ||
+                splittata[0].charAt(3) < 48 || splittata[0].charAt(3) > 57 ||
+                splittata[0].charAt(4) < 48 || splittata[0].charAt(4) > 57) {
+            view.showError("Codice istituto non valido");
+            return;
+        }
+
         String[] split = splittata[3].split("/");
         model.aggiungi(new Richiesta(splittata[0], Integer.parseInt(splittata[1]), splittata[2],
                 LocalDate.of(Integer.parseInt(split[2]), Integer.parseInt(split[1]), Integer.parseInt(split[0]))));
