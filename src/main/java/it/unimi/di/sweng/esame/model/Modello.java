43c9f801855734a2f1a76474bfcb113fac2d4f0b
Daniele Reccagni <daniele.reccagni1@studenti.unimi.it>
2023-09-06 16:29:55 +0200

    VERDE: implementato test testAggiungi

--- src/main/java/it/unimi/di/sweng/esame/model/Modello.java
+++ src/main/java/it/unimi/di/sweng/esame/model/Modello.java
@@ -46,7 +46,20 @@ public class Modello implements Observable<Map<String, Richiesta>> {
       obs.update(this);
   }
 
-  public void aggiungi(@NotNull Richiesta richiesta) {
+  public void aggiungi(@NotNull Richiesta r) {
+    StringBuilder s = new StringBuilder();
+    if (r.data().getDayOfMonth() < 10)
+      s.append("0");
+    s.append(r.data().getDayOfMonth());
+    s.append("/");
+    if (r.data().getMonth().getValue() < 10)
+      s.append("0");
+    s.append(r.data().getMonth().getValue());
+    s.append("/");
+    s.append(r.data().getYear());
 
+    richieste.put(r.codice() + " " + s, r);
+
+    notifyObservers();
   }
 }
