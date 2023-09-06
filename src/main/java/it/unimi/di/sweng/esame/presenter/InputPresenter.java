bea40707e0835ebc8009c3d96ea33bdba440fa39
Daniele Reccagni <daniele.reccagni1@studenti.unimi.it>
2023-09-06 16:44:18 +0200

    VERDE: implementato test testActionInserisci

--- src/main/java/it/unimi/di/sweng/esame/presenter/InputPresenter.java
+++ src/main/java/it/unimi/di/sweng/esame/presenter/InputPresenter.java
@@ -1,8 +1,11 @@
 package it.unimi.di.sweng.esame.presenter;
 
 import it.unimi.di.sweng.esame.model.Modello;
+import it.unimi.di.sweng.esame.model.Richiesta;
 import it.unimi.di.sweng.esame.views.USRView;
 import org.jetbrains.annotations.NotNull;
+
+import java.time.LocalDate;
 
 public class InputPresenter implements Presenter {
     private final @NotNull Modello model;
@@ -15,6 +18,11 @@ public class InputPresenter implements Presenter {
 
     @Override
     public void action(String text1, String text2) {
-
+        if (text1.equals("") || text2.equals("")) throw new IllegalArgumentException();
+        String[] splittata = text2.split(":");
+        String[] split = splittata[3].split("/");
+        model.aggiungi(new Richiesta(splittata[0], Integer.parseInt(splittata[1]), splittata[2],
+                LocalDate.of(Integer.parseInt(split[2]), Integer.parseInt(split[1]), Integer.parseInt(split[0]))));
+        view.showSuccess();
     }
 }
